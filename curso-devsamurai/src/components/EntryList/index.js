a7478ec9a2590acfc88077e7443cd78a2e84e5d4
Luiz Pires <lutidev@gmail.com>
2020-07-28 11:32:10 -0300

    list entries

--- curso-devsamurai/src/components/EntryList/index.js
+++ curso-devsamurai/src/components/EntryList/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, FlatList, StyleSheet} from 'react-native';
 
 import EntryListItem from './EntryListItem';
 
-export default function EntryList({entries}) {
+import {getEntries} from '../../services/Entries';
+
+export default function EntryList() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    async function loadEntries() {
+      const data = await getEntries();
+      setEntries(data);
+    }
+
+    loadEntries();
+
+    console.log('EntryList :: useEffect');
+  }, []);
+
   return (
     <View>
       <Text style={styles.title}>Últimos Lançamentos</Text>
