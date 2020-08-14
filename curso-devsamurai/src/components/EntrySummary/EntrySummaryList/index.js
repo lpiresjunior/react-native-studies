1fdb7ee51a49b972092f524f05c1a7c9edfe05ec
Luiz Pires <lutidev@gmail.com>
2020-08-14 08:47:24 -0300

    adjust main page

--- curso-devsamurai/src/components/EntrySummary/EntrySummaryList/index.js
+++ curso-devsamurai/src/components/EntrySummary/EntrySummaryList/index.js
@@ -4,8 +4,6 @@ import {View, Text, FlatList, StyleSheet} from 'react-native';
 export default function EntrySummaryList({entriesGrouped}) {
   return (
     <View>
-      <Text style={styles.title}>Categorias</Text>
-
       <FlatList
         data={entriesGrouped}
         renderItem={({item}) => (
@@ -21,11 +19,5 @@ export default function EntrySummaryList({entriesGrouped}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    marginTop: 10,
-    marginBottom: 10,
   },
 });
