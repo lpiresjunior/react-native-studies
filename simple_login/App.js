d720b4ac5952c3e2cd12fe0e57ed71010a77f7b3
Luiz Pires <luizluti@outlook.com>
2020-09-20 23:53:04 -0300

    simple login

--- simple_login/App.js
+++ simple_login/App.js
@@ -1,114 +1,113 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from 'react'
+import { SafeAreaView, View, Text, TextInput, Button, StyleSheet } from 'react-native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = () => {
+  // email: suporte@b7web.com.br
+  // senha: 1234
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-const App: () => React$Node = () => {
+  const [status, setStatus] = useState('')
+  const [showCupom, setShowCupom] = useState(false)
+
+  const handleVerifyLogin = async () => {
+    setStatus('')
+    setShowCupom(false)
+
+    // eslint-disable-next-line no-undef
+    const req = await fetch('https://api.b7web.com.br/loginsimples/', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await req.json()
+
+    if (json.status === 'ok') {
+      setStatus('Acesso LIBERADO!')
+      setShowCupom(true)
+    } else {
+      setStatus('Acesso Negado')
+      setShowCupom(false)
+    }
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Super Cupom</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Digite seu e-mail'
+        value={email}
+        onChangeText={t => setEmail(t)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder='Digite sua senha'
+        value={password}
+        onChangeText={t => setPassword(t)}
+        secureTextEntry={true}
+      />
+
+      <Button title='Entrar' onPress={handleVerifyLogin} />
+
+      <Text style={styles.status}>{status}</Text>
+
+      {showCupom &&
+        <View style={styles.cupomArea}>
+          <Text style={styles.cupomTitle}>Código de Cupom:</Text>
+          <Text style={styles.cupomCode}>JDHD743F</Text>
+        </View>
+      }
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    padding: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  header: {
+    color: '#FFF',
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 30
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  input: {
+    height: 45,
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+    color: '#FFF',
+    backgroundColor: '#555',
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10
   },
-  highlight: {
-    fontWeight: '700',
+  status: {
+    margin: 50,
+    color: '#FFF',
+    fontSize: 18,
+    textAlign: 'center'
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  cupomArea: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 30
   },
-});
+  cupomTitle: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  cupomCode: {
+    textAlign: 'center',
+    fontSize: 40
+  }
+})
 
-export default App;
+export default App
---
+++ simple_login/README.md
@@ -0,0 +1,6 @@
+### Login simples
+
+Um sistema de Login simples feito durante o curso de react native da B7web
+
+- **email**: suporte@b7web.com.br
+- **senha**: 1234
