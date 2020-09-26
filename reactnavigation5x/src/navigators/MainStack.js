b80f711b86a6b51ea108444c52b7a2990c0cb19a
Luiz Pires <luizluti@outlook.com>
2020-09-26 15:20:56 -0300

    inserindo elementos no header 1

--- reactnavigation5x/src/navigators/MainStack.js
+++ reactnavigation5x/src/navigators/MainStack.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from '../pages/HomeScreen';
 import AboutScreen from '../pages/AboutScreen';
+import {Button, Text, Image} from 'react-native';
 
 const MainStack = createStackNavigator();
 
@@ -19,7 +20,19 @@ export default () => {
           fontSize: 16,
         },
       }}>
-      <MainStack.Screen name="Home" component={HomeScreen} />
+      <MainStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: () => (
+            <Image
+              style={{width: 150, height: 45}}
+              source={{uri: 'https://www.google.com.br/google.jpg'}}
+            />
+          ),
+          headerRight: () => <Button title="opa" onPress={() => alert('oi')} />,
+        }}
+      />
       <MainStack.Screen name="About" component={AboutScreen} />
     </MainStack.Navigator>
   );
